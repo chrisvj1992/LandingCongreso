db6513022c108f4ed3516007759aa1258703cfaf
chrisvj1992 <chrisvega@live.com.mx>
2024-08-01 11:27:03 -0600

    header fixed

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <>
     <Header/>
-
-    <Hero />
-    <VisMis/>
+    <Hero/>    
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Logo from "../../assets/images/logo.png";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
+
 
 const Header = () => {
+    const [showMenu, setShowMenu] = useState(false);
     return (
-    <header className="flex items-center p-4 w-full h-[14vh] bg-tertiaryColor border" >
-        <div className="xl:m-20 w-1/6 items-center">
+    <header className="flex bg-tertiaryColor items-center justify-between xl:justify-start w-full py-4 px-8 h-[14vh] z-50 " >
+        <div className="xl:w-1/6 w-1/2 text-center">
             <a href="#"><img className="" src={Logo} alt="Logo" /></a>
         </div>
-        <nav className="fixed w-full h-full right-0 top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10" >
+        
+        <div className={`fixed absolute top-0 left-0 h-full hidden w-[50%] bg-tertiaryColor`}></div>
+        <div className={`fixed absolute top-0 left-0 h-full hidden w-[60%] bg-tertiaryColor`}></div>
+        <nav className={`fixed bg-tertiaryColor w-[80%] md:w-[80%] xl:w-full h-full ${showMenu ? 'left-10 bg-opacity-60' : '-left-full bg-opacity-100'} top-0  xl:static flex-1 flex flex-col xl:flex-row items-center justify-center gap-10 transition-all duration-500`} >
             <a href="#" class="bebas-neue-regular" className="text-2xl font-medium text-primaryColor">
                 Ponentes
             </a>
@@ -23,8 +29,10 @@ const Header = () => {
             <a href="#" class="bebas-neue-regular" className="text-2xl font-medium text-primaryColor">
                 Contacto
             </a>
-
         </nav>
+        
+        <button onClick={() => setShowMenu(!showMenu)} className={`md:hidden text-2xl p-2 ${showMenu ? '' :'hidden' }`} ><IoMdClose /></button>
+        <button onClick={() => setShowMenu(!showMenu)} className={`xl:hidden text-2xl p-2 ${showMenu ? 'hidden' :'' }`}><IoMdMenu /></button>
     </header>
     );
 };
